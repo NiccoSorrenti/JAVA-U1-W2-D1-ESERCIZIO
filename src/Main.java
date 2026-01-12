f4a128cfdb8ce7142cb519d158d18e74148afeea
Niccolò <niccolo.sorrenti@gmail.com>
2026-01-12 16:47:53 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        int[] array = new int[5];
+        Scanner scanner = new Scanner(System.in);
+
+        // Inizializzazione array con valori casuali tra 1 e 10
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
         }
+
+        stampaArray(array);
+
+        while (true) {
+            System.out.print("\nInserisci un numero (0 per terminare): ");
+            int valore = scanner.nextInt();
+
+            if (valore == 0) {
+                System.out.println("Programma terminato.");
+                break;
+            }
+
+            System.out.print("Inserisci la posizione (0-4): ");
+            int posizione = scanner.nextInt();
+
+            try {
+                array[posizione] = valore;
+                stampaArray(array);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Errore: posizione non valida. Inserire un indice tra 0 e 4.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    // Metodo per stampare l'array
+    private static void stampaArray(int[] array) {
+        System.out.print("Stato dell'array: ");
+        for (int numero : array) {
+            System.out.print(numero + " ");
+        }
+        System.out.println();
+
     }
 }
